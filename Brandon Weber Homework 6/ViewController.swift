c1a17fe10366b663caaf4656992c2043b0d564e0
Brandon Weber <bweber425@gmail.com>
2023-04-11 15:15:41 -0400

    Initial Commit

---
+++ Brandon Weber Homework 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brandon Weber Homework 6
+//
+//  Created by Brandon Weber on 4/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
